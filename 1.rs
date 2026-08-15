85245c580696f3ece7a19a335652a012ba4fba93
vynxidn <150652521+vynnxidn@users.noreply.github.com>
2026-08-15 21:37:21 +0700

    Add files via upload

---
+++ 1.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("hello vynxidn");
+    println!("==================================================");
+    println!("perkenalkan nama saya vyn dari universitas islam madura");
+    println!("jurusan teknik informatika angkatan 24");
+    println!("saya berasal dari sebuah wilayah di pulau madura");
+    println!("wilayah tersebut bisa di sebut 'pamekasan'");
+    println!("==================================================");
+}
