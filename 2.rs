cfbdc9112f6c5f7567d4653732a88e4ccb5390bd
vynxidn <150652521+vynnxidn@users.noreply.github.com>
2026-08-16 00:45:40 +0700

    Add files via upload

--- 2.rs
+++ 2.rs
@@ -1,3 +1,8 @@
+use std::process::Command;
+
+
+
+
 fn main() {
     println!("welcome back vynxidn");
 
@@ -12,5 +17,15 @@ fn main() {
                                                                                                               
     ";
     println!("{}", banner);
+    let uname = Command::new("uname")
+    .arg("-a")
+    .output()
+    .expect("gagal menjalan uname -a ");
+    println!("nama sysytem: {}", String::from_utf8_lossy(&uname.stdout));
+
+    let code1rs = Command::new("./1")
+    .output()
+    .expect("gagal menjalankan ./1");
+    println!("{}", String::from_utf8_lossy(&code1rs.stdout))
 
 }
---
+++ 3.rs
@@ -0,0 +1,9 @@
+fn main(){
+	println!("welcome to vynxidn project 03");
+	let x = 9;
+	let y = 9;
+	let  a = x + y;
+	println!("nilai x = {}", x);
+	println!("nilai y = {}", y);
+	println!("nilai a = x + y = {}", a)
+}
